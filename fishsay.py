dbd7b14d365631693305b9b46b639a0bc3b26acd
NovaLovesBella <67805141+NovaLovesBella@users.noreply.github.com>
2020-10-06 13:23:31 -0400

    Converted it to python
    I converted it from c to python and made you able to choose character length

---
+++ fishsay.py
@@ -0,0 +1,16 @@
+charlength = input("How many characters do you want to use:\n")
+intlength = charlength
+fishy = '           <॰)))><'
+fishsay = input('What you want to say: \n')
+if len(fishsay) > int(intlength) :
+  print('Thats to long')
+  exit()
+elif len(fishsay) < int(intlength) :
+  print('')
+for i in range(6) :
+  print('hlughlughlughlug')
+if len(fishsay) < int(intlength) :
+  print(fishsay)
+print("        ༄")
+print(fishy)
+	
